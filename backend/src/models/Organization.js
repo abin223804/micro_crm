1b77709d90443c617700d61b42a24fc1d69ceda5
abinschandran <abinschandran@gmail.com>
2025-11-30 12:53:30 +0530

    Organization_schema created

---
+++ backend/src/models/Organization.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const OrganizationSchema = new mongoose.Schema({
+    name:{
+        type:String,required:true
+    }
+},{timestamps:true})
+
+export default mongoose.model(OrganizationSchema,OrganizationSchema)
