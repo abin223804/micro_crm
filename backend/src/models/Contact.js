9ccce92c65affc875fd5d3d0cb44acd29724ac85
abinschandran <abinschandran@gmail.com>
2025-11-30 13:13:17 +0530

    contact_schema added

---
+++ backend/src/models/Contact.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const ContactSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String },
+    notes: { types: String },
+
+    organizationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Organization",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Contact", ContactSchema);
